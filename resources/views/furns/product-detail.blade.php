654cec10a9aaa4fb5d7e3f347dc9750dd8e6e635
Đào Thanh Tùng <tungdt2@bambooairways.com>
2021-06-15 09:46:06 +0700

    update

--- resources/views/furns/product-detail.blade.php
+++ resources/views/furns/product-detail.blade.php
@@ -8,7 +8,7 @@
 
     <div id="fullpage" class="">
         <div class="section pd-0" id="products">
-            <div class="container-fluid pd-0 bg-white h-100" style="margin-top: 70px;">
+            <div class="container-fluid pd-0 bg-white h-100" style="margin-top: 80px;">
                 <div class="row">
                     <div class="col-lg-12" style="padding: 20px 20px;">
                         <h6 style="color: #646464 !important; text-align: left">
@@ -20,51 +20,63 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-lg-6" style="height: 100%; overflow: hidden !important; padding: 20px !important;">
-                        <img class="image-avatar" src="{{ $product->avatar }}" alt="" style="max-height: 750px; width: 100%;">
-                    </div>
-                    <div class="col-lg-1" style="height: 100%; overflow: hidden !important; padding: 20px !important;">
-                        @if ($product->pictures != null && is_array($product->pictures))
-                            @foreach ($product->pictures as $key => $picture)
-                                <img class="image-detail"
-                                src="{{ asset('uploads/'.$picture) }}" alt=""
-                                style="
-                                width: 85px;
-                                height: 85px;
-                                margin-bottom: 10px;
-                                border: 1px solid #c7c7c7;
-                                cursor: pointer;">
-                            @endforeach
-                        @endif
-                    </div>
-                    <div class="col-lg-5" style="color: #646464;
-                    text-align: left; height: 100%; overflow: hidden !important; padding: 20px !important;">
+                    <div class="col-lg-2"></div>
+                    <div class="col-lg-8">
                         <div class="row">
-                            <div class="col-lg-6">
-                                {{ $product->category->name }}
+                            <div class="col-lg-6" style="height: 100%; overflow: hidden !important; padding: 20px !important;">
+                                <img id="myImg" class="image-avatar" src="{{ $product->avatar }}" alt="" style="max-height: 750px; width: 100%; max-width: 750px;">
                             </div>
-                            <div class="col-lg-6 text-right">
-                                {{ $product->code }}
+                            <div class="col-lg-1" style="height: 100%; overflow: hidden !important; padding: 20px !important;">
+                                @if ($product->pictures != null && is_array($product->pictures))
+                                    @foreach ($product->pictures as $key => $picture)
+                                        <img class="image-detail"
+                                        src="{{ asset('uploads/'.$picture) }}" alt=""
+                                        style="
+                                        width: 100%;
+                                        height: auto;
+                                        margin-bottom: 10px;
+                                        border: 1px solid #c7c7c7;
+                                        cursor: pointer;
+                                        display: block">
+                                    @endforeach
+                                @endif
                             </div>
-                        </div>
-                        <br>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <h3 class="text-uppercase">{{ $product->name }}</h3>
-                            </div>
-                            <div class="col-lg-12">
+                            <div class="col-lg-5" style="color: #646464;
+                            text-align: left; height: 100%; overflow: hidden !important; padding: 20px !important;">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        {{ $product->category->name }}
+                                    </div>
+                                    <div class="col-lg-6 text-right">
+                                        {{ $product->code }}
+                                    </div>
+                                </div>
                                 <br>
-                                <h4 class="text-uppercase" style="font-weight: 400; color: rgb(172, 83, 83);">Liên hệ</h4>
-                            </div>
-                            <div class="col-lg-12">
-                                {!! $product->description !!}
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <h3 class="text-uppercase">{{ $product->name }}</h3>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <br>
+                                        <h4 class="text-uppercase" style="font-weight: 400; color: rgb(172, 83, 83);">Liên hệ</h4>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        {!! $product->description !!}
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
 
+        <div id="myModal" class="modal">
+            <span class="close">&times;</span>
+            <img class="modal-content" id="img01">
+            <div id="caption"></div>
+        </div>
         <br><br><br>
     </div>
 
@@ -80,6 +92,26 @@
             let src = $(this).attr('src');
             $('.image-avatar').attr('src', src);
         })
+
+        var modal = document.getElementById("myModal");
+
+        // Get the image and insert it inside the modal - use its "alt" text as a caption
+        var img = document.getElementById("myImg");
+        var modalImg = document.getElementById("img01");
+        var captionText = document.getElementById("caption");
+        img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+        modal.style.display = "none";
+        }
     </script>
   </body>
 </html>
